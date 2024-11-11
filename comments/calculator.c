c8206ae7548e8fc58524133bd6f60583478739ff
Gavin Coulson <gavincoulson@Gavins-MacBook-Pro.local>
2024-11-11 08:24:21 -0700

    fun times

---
+++ comments/calculator.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("\n%d", 5 + 2);
+    printf("\n%d", 5 - 2);
+    printf("\n%d", 5 / 2);
+    printf("\n%d", 5 * 2);
+    printf("\n%d", 5 % 2);
+
+    return 0;
+}
---
+++ comments/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    // This progrmam doesn't really do anything
+    printf("\n Gavin Coulson\n 30 \n Male");
+
+    printf("\n  *\n ***\n*****\n");
+
+    printf("I am %d years old\n", 21);
+
+    printf("I am %d today and tomorrow I will be %d\n", 20, 21);
+
+    printf("My average grade: %.1f\n", 93.7);
+    return 0;
+}
