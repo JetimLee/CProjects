3bbc6230c13e4055c1909635725f2c30163ff9c8
Gavin Coulson <gavincoulson@Gavins-MacBook-Pro.local>
2024-11-11 08:48:53 -0700

    fun times

---
+++ variables/perimeter.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    double height;
+    double width;
+
+    printf("Enter the rectangle height: ");
+    scanf("%lf", &height);
+
+    printf("Enter the rectangle width: ");
+    scanf("%lf", &width);
+
+    printf("The area of the rectangle is: %.2f\n", (2 * (width + height)));
+
+    return 0;
+}
