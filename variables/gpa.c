f4dd0911ad94cbecd9f544cf9a7d71400cd8761a
JetimLee <gavincoulson1@gmail.com>
2024-11-11 09:09:33 -0700

    some notes

--- variables/gpa.c
+++ variables/gpa.c
@@ -7,12 +7,16 @@ int main()
     printf("Enter the first grade: ");
     scanf("%d", &grade1);
 
+    double gpa;
+    // printf("Enter your GPA: ");
+    // scanf("%lf", &gpa); // Correct for double, incorrect for float
+
     printf("Enter the second grade: ");
     scanf("%d", &grade2);
 
     double gpa = ((double)grade1 + grade2) / 2;
 
-    printf("Your gpa is: %f\n", (gpa));
+    printf("Your gpa is: %lf\n", (gpa));
 
     return 0;
 }
