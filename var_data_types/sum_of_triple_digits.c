77ef3eb7fdfd3bd58f25760e797f6fb3ac7b889e
JetimLee <gavincoulson1@gmail.com>
2024-11-28 19:59:27 -0700

    comments for fun

--- var_data_types/sum_of_triple_digits.c
+++ var_data_types/sum_of_triple_digits.c
@@ -8,6 +8,8 @@ int main()
     printf("Enter a number that has 3 digits such as 531 to get the sum of the numbers:  ");
     scanf("%d", &receivedThreeDigitNumber);
 
+    // Can also do units = num % 10, tens = (num/10) % 10, but this works too
+
     int hundredsDigit = receivedThreeDigitNumber / 100;                                     // 431 gives 4
     int tensDigit = (receivedThreeDigitNumber - (100 * hundredsDigit)) / 10;                // 431 - 400 = 31 / 10 = 3
     int singlesDigit = (receivedThreeDigitNumber - (100 * hundredsDigit)) - 10 * tensDigit; // 431 - 400 = 31 - 30 = 1
