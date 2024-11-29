7bb227facc6e9b8828d87fcc29b3dce5f22f95f6
JetimLee <gavincoulson1@gmail.com>
2024-11-28 19:04:06 -0700

    changes

--- var_data_types/seconds_to_minutes_to_hours.c
+++ var_data_types/seconds_to_minutes_to_hours.c
@@ -8,8 +8,8 @@ int main()
     scanf("%d", &receivedSeconds);
 
     int hours = receivedSeconds / 3600;
-    int minutes = (receivedSeconds - 3600) / 60;
-    int seconds = (receivedSeconds - 3600) % 60;
+    int minutes = (receivedSeconds - hours * 3600) / 60;
+    int seconds = (receivedSeconds - hours * 3600) % 60;
 
     printf("%d :%d : %d ", hours, minutes, seconds);
 
