e455d98f11be74e37668ecc506e9b77b20e66bf0
JetimLee <gavincoulson1@gmail.com>
2024-11-28 19:00:10 -0700

    converting

---
+++ var_data_types/seconds_to_minutes_to_hours.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int receivedSeconds;
+
+    printf("Enter an amount of seconds to convert to hours:minutes:seconds format ");
+    scanf("%d", &receivedSeconds);
+
+    int hours = receivedSeconds / 3600;
+    int minutes = (receivedSeconds - 3600) / 60;
+    int seconds = (receivedSeconds - 3600) % 60;
+
+    printf("%d :%d : %d ", hours, minutes, seconds);
+
+    return 0;
+}
